acb50e7f066333e00ca23a7bdb8c9fecdd503f1f
cybai <cyb.ai.815@gmail.com>
2023-12-02 15:36:44 +0900

    Implement day 2 part 2

--- rust/day02/src/main.rs
+++ rust/day02/src/main.rs
@@ -8,6 +8,7 @@ fn main() -> Result<(), ()> {
     let games = parse(&input);
 
     println!("Part 1: {}", part1(&games));
+    println!("Part 2: {}", part2(&games));
 
     Ok(())
 }
@@ -16,6 +17,7 @@ type GameID = u32;
 
 #[derive(Debug)]
 struct Game {
+    #[allow(dead_code)]
     id: GameID,
     r: u32,
     g: u32,
@@ -85,6 +87,10 @@ fn part1(games: &HashMap<GameID, Game>) -> u32 {
         .sum()
 }
 
+fn part2(games: &HashMap<GameID, Game>) -> u32 {
+    games.iter().map(|(_, game)| game.r * game.g * game.b).sum()
+}
+
 #[test]
 fn test_part1() {
     let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -95,4 +101,16 @@ fn test_part1() {
 
     let games = parse(input);
     assert_eq!(part1(&games), 8);
+}
+
+#[test]
+fn test_part2() {
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    let games = parse(input);
+    assert_eq!(part2(&games), 2286);
 }
