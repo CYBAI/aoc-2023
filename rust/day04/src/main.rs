f9f71cfb6fe0442e31470fe25b27619338f72d1f
cybai <cyb.ai.815@gmail.com>
2023-12-04 20:14:01 +0900

    Implement day 4 part 2

--- rust/day04/src/main.rs
+++ rust/day04/src/main.rs
@@ -22,6 +22,7 @@ fn main() -> Result<(), ()> {
     let cards = parse(&input);
 
     println!("Part 1: {}", part1(&cards));
+    println!("Part 2: {}", part2(&cards));
 
     Ok(())
 }
@@ -75,6 +76,21 @@ fn part1(cards: &Vec<Card>) -> u32 {
         .sum()
 }
 
+fn part2(cards: &Vec<Card>) -> u32 {
+    let boundary = cards.len();
+    let mut ans = vec![1; boundary];
+
+    for (id, card) in cards.iter().enumerate() {
+        let n = card.winning.intersection(&card.holds).count();
+
+        for i in (id + 1)..=(id + n) {
+            ans[i] += ans[id];
+        }
+    }
+
+    ans.iter().sum()
+}
+
 #[test]
 fn test_part1() {
     let input = "
@@ -87,4 +103,18 @@ fn test_part1() {
 
     let cards = parse(&input);
     assert_eq!(part1(&cards), 13);
+}
+
+#[test]
+fn test_part2() {
+    let input = "
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    let cards = parse(&input);
+    assert_eq!(part2(&cards), 30);
 }
