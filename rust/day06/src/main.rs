65caa9b3684ad4e838bef1da63175f88d02d1940
cybai <cyb.ai.815@gmail.com>
2023-12-06 23:17:07 +0900

    Implement day 6 part 2

--- rust/day06/src/main.rs
+++ rust/day06/src/main.rs
@@ -1,10 +1,11 @@
-type Race = (u32, u32);
+type Race = (u64, u64);
 
 fn main() {
     let input = include_str!("./input");
     let races = parse_input(input);
 
     println!("Part 1: {}", part1(&races));
+    println!("Part 2: {}", part2(&races));
 }
 
 fn parse_input(input: &str) -> Vec<Race> {
@@ -19,7 +20,7 @@ fn parse_input(input: &str) -> Vec<Race> {
                 Some(
                     line.split_whitespace()
                         .skip(1)
-                        .map(|n| n.parse::<u32>().unwrap())
+                        .map(|n| n.parse::<u64>().unwrap())
                         .collect::<Vec<_>>(),
                 )
             }
@@ -29,26 +30,45 @@ fn parse_input(input: &str) -> Vec<Race> {
     std::iter::zip(parsed[0].clone(), parsed[1].clone()).collect()
 }
 
-fn part1(races: &[Race]) -> u32 {
-    races.iter().fold(1, |acc, (time, distance)| {
-        let lower = 1;
-        let upper = time - 1;
-        let mid = (lower + upper) / 2;
+fn part1(races: &[Race]) -> u64 {
+    races
+        .iter()
+        .fold(1, |acc, race| acc * find_winning_way(*race))
+}
 
-        let count = (lower..=mid)
-            .filter_map(|x| {
-                let diff = time - x;
+fn part2(races: &[Race]) -> u64 {
+    let (time, distance) = races.iter().fold(
+        (String::new(), String::new()),
+        |(fixed_fst, fixed_snd), (fst, snd)| {
+            (
+                format!("{}{}", fixed_fst, fst),
+                format!("{}{}", fixed_snd, snd),
+            )
+        },
+    );
 
-                if diff * x > *distance {
-                    Some(if diff == x { 1 } else { 2 })
-                } else {
-                    None
-                }
-            })
-            .sum::<u32>();
+    let time = time.parse::<u64>().unwrap();
+    let distance = distance.parse::<u64>().unwrap();
 
-        acc * count
-    })
+    find_winning_way((time, distance))
+}
+
+fn find_winning_way((time, distance): Race) -> u64 {
+    let lower = 1;
+    let upper = time - 1;
+    let mid = (lower + upper) / 2;
+
+    (lower..=mid)
+        .filter_map(|x| {
+            let diff = time - x;
+
+            if diff * x > distance {
+                Some(if diff == x { 1 } else { 2 })
+            } else {
+                None
+            }
+        })
+        .sum::<u64>()
 }
 
 #[cfg(test)]
@@ -63,7 +83,12 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = parse_input(EXAMPLE);
-
         assert_eq!(part1(&input), 288);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse_input(EXAMPLE);
+        assert_eq!(part2(&input), 71503);
     }
 }
