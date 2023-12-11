ad5d9f6e1454f5cf8dfb21800819b8d8217702ac
cybai <cyb.ai.815@gmail.com>
2023-12-11 22:28:30 +0900

    Implement day 9 part 1

---
+++ rust/day09/src/main.rs
@@ -0,0 +1,70 @@
+fn main() {
+    let input = include_str!("./input");
+    let histories = parse(input);
+
+    println!("Part 1: {}", part1(&histories));
+}
+
+type History = Vec<i64>;
+
+fn parse(input: &str) -> Vec<History> {
+    input
+        .lines()
+        .filter_map(|line| {
+            let line = line.trim();
+
+            if line.is_empty() {
+                None
+            } else {
+                Some(
+                    line.split_whitespace()
+                        .map(|s| s.parse().unwrap())
+                        .collect::<History>(),
+                )
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn part1(histories: &[History]) -> i64 {
+    histories
+        .iter()
+        .map(|history| find_next_num(&history))
+        .sum()
+}
+
+fn find_next_num(history: &[i64]) -> i64 {
+    if history.iter().all(|n| *n == 0) {
+        return 0;
+    }
+
+    let diff = calc_diff(history);
+    history.last().unwrap() + find_next_num(&diff)
+}
+
+fn calc_diff(history: &[i64]) -> History {
+    let mut diff = Vec::new();
+
+    for i in 1..history.len() {
+        diff.push(history[i] - history[i - 1]);
+    }
+
+    diff
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+    ";
+
+    #[test]
+    fn test_part1() {
+        let histories = parse(EXAMPLE_INPUT);
+        assert_eq!(part1(&histories), 114);
+    }
+}
